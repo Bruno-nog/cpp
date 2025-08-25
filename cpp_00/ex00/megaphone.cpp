538efb2cdfe785080deb0f59b14e05fa824a8b09
Bruno Nogueira De Queiroz <bqueiroz.nog@gmail.com>
2025-08-25 18:30:02 -0300

    First commit

---
+++ cpp_00/ex00/megaphone.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brunogue <brunogue@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/22 13:59:34 by brunogue          #+#    #+#             */
+/*   Updated: 2025/08/25 17:41:40 by brunogue         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+void print_in_uppercase(char *input, int i)
+{
+	if (!input)
+	return ;
+	if (i > 0)
+		std::cout << " ";
+	int j = 0;
+	while (input)
+	{
+		std::cout << toupper(input[i]);
+		i++;
+	}
+}
+
+int main (int ac, char **av)
+{
+	int i = 0;
+	
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+	else
+	{
+		while (++i < ac)
+			print_in_uppercase(av[i], i);
+	}
+	return (0);
+}
