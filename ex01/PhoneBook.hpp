7cd0c0cad9f399329c6923f9c129879ff829cb50
root <bqueiroz.nog@gmail.com>
2025-09-14 19:02:31 -0300

    test

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -1,12 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brunogue <brunogue@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 17:16:20 by brunogue          #+#    #+#             */
-/*   Updated: 2025/09/01 17:16:21 by brunogue         ###   ########.fr       */
+/*   Updated: 2025/09/14 17:53:16 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+#include <iostream>
+#include "Contact.hpp"
+
+class PhoneBook
+{
+    private:
+        Contact contactList[8];
+        int     currentIndex;
+        int     totalContacts;
+
+    public:
+        PhoneBook();
+        
+        void    addContact();
+        void    searchContact() const;
+
+        std::string truncateString(const std::string& str) const;
+        void    printFormatted(const std::string& str) const;
+};
+
+#endif
