d185675b1c6b270b73ed11126d365fe63ac8b1b4
root <bqueiroz.nog@gmail.com>
2025-09-14 23:00:35 -0300

    working on phonebook.cpp

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -1,12 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brunogue <brunogue@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/25 19:11:40 by brunogue          #+#    #+#             */
-/*   Updated: 2025/08/25 19:11:48 by brunogue         ###   ########.fr       */
+/*   Updated: 2025/09/14 20:20:57 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PhoneBook.hpp"
+#include <iostream>
+#include <sstream>
+
+PhoneBook::PhoneBook()
+{
+    contactCount = 0;
+    totalContacts = 0;
+}
+
+void PhoneBook::phoneBook_ADD()
+{
+    
+}
