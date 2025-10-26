13cf0162da96aa9bfc3e1b477f7a07850af5b8fd
Bruno Nogueira De Queiroz <bqueiroz.nog@gmail.com>
2025-10-26 18:15:38 -0300

    file opened ex04

--- cpp01/ex04/Main.cpp
+++ cpp01/ex04/Main.cpp
@@ -6,36 +6,33 @@
 /*   By: brunogue <brunogue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 18:52:07 by brunogue          #+#    #+#             */
-/*   Updated: 2025/10/26 17:47:24 by brunogue         ###   ########.fr       */
+/*   Updated: 2025/10/26 18:14:59 by brunogue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
-#include <sys/stat.h>
-#include <fcntl.h>
-
 
 int main(int ac, char **av)
 {
-	const char* filename = av[1];
-	int fd;
+	
+	if (ac != 4)
+	{
+		std::cout << "Usage: ./sed <filename> <s1> <s2>" << std::endl;
+		return (1);
+	}
+	std::string filename = av[1];
+	std::string s1 = av[2];
+	std::string s2 = av[3];
 
-	if (ac > 4)
+	std::ifstream infile(filename.c_str(), std::ios::binary);
+	if (!infile)
 	{
-		std::cout << "Many arguments" << std::endl;
-	} else if (ac <= 3)
+		std::cerr << "Error: cannot open input file " << filename << std::endl;
+		return (1);
+	} else
 	{
-		std::cout << "Few arguments" << std::endl;
+		std::cout << "abriu\n";
 	}
 
-	try
-	{
-		fd = open(filename, O_WRONLY);
-	}
-	catch (!fd)
-	{
-		std::cout << "algo de errado nao esta certo\n";
-		return (0);
-	}
 }
