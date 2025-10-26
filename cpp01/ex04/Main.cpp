c6a44003b93b470efbfb545ecdb45f2a56fbd7ba
Bruno Nogueira De Queiroz <bqueiroz.nog@gmail.com>
2025-10-26 19:57:30 -0300

    ex04 done i gues

--- cpp01/ex04/Main.cpp
+++ cpp01/ex04/Main.cpp
@@ -6,19 +6,19 @@
 /*   By: brunogue <brunogue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 18:52:07 by brunogue          #+#    #+#             */
-/*   Updated: 2025/10/26 18:14:59 by brunogue         ###   ########.fr       */
+/*   Updated: 2025/10/26 19:54:14 by brunogue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 int main(int ac, char **av)
 {
-	
 	if (ac != 4)
 	{
-		std::cout << "Usage: ./sed <filename> <s1> <s2>" << std::endl;
+		std::cerr << "Usage: ./sed <filename> <s1> <s2>" << std::endl;
 		return (1);
 	}
 	std::string filename = av[1];
@@ -32,7 +32,33 @@ int main(int ac, char **av)
 		return (1);
 	} else
 	{
-		std::cout << "abriu\n";
-	}
+		std::string content;
+		{
+			std::ostringstream ss;
+			ss << infile.rdbuf();
+			content = ss.str();
+		}
+		infile.close();
+		std::string res;
+		size_t pos = 0;
+		size_t found;
+		while ((found = content.find(s1, pos)) != std::string::npos)
+		{
+			res.append(content, pos, found - pos);
+			res.append(s2);
+			pos = found + s1.length();
+		}
+		res.append(content, pos, content.size() - pos);
 
+		std::string outname = filename + ".replace";
+		std::ofstream outfile(outname.c_str(), std::ios::binary);
+		if (!outfile)
+		{
+			std::cerr << "Error: cannot create output file." << std::endl;
+			return (1);
+		}
+		outfile << res;
+		outfile.close();
+	}
+	return (0);
 }
