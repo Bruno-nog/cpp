dbe47e7f5400c3aa8549da0a8616e26ed24d7f40
Bruno Nogueira De Queiroz <bqueiroz.nog@gmail.com>
2025-10-27 15:43:21 -0300

    working on ex05

--- cpp01/ex05/Main.cpp
+++ cpp01/ex05/Main.cpp
@@ -5,34 +5,19 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: brunogue <brunogue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/24 17:04:04 by brunogue          #+#    #+#             */
-/*   Updated: 2025/10/27 15:31:43 by brunogue         ###   ########.fr       */
+/*   Created: 2025/10/27 14:51:42 by brunogue          #+#    #+#             */
+/*   Updated: 2025/10/27 15:42:54 by brunogue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include "Weapon.hpp"
-#include "HumanA.hpp"
-#include "HumanB.hpp"
 
-int main()
+int main (int ac, char **av)
 {
-	{
-		Weapon club = Weapon("crude spiked club");
 
-		HumanA bob("Bob", club);
-		bob.attack();
-		club.setType("some other type of club");
-		bob.attack();
-	}
+	if (ac != 2)
 	{
-		Weapon club = Weapon("crude spiked club");
-		HumanB jim("jim");
+		std::cout << "something\n";
 		
-		jim.setWeapon(&club);
-		jim.attack();
-		club.setType("some other type of club");
-		jim.attack();
 	}
-	std::cout << std::endl;
 }
