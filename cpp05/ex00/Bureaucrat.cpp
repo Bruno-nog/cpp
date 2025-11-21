86e6da2796a921761e36ba4419fc2a073c61f62e
Bruno Nogueira De Queiroz <bqueiroz.nog@gmail.com>
2025-11-21 15:23:31 -0300

    added cpp05, working on ex00

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -6,19 +6,16 @@
 /*   By: brunogue <brunogue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 19:02:08 by brunogue          #+#    #+#             */
-/*   Updated: 2025/11/05 19:11:03 by brunogue         ###   ########.fr       */
+/*   Updated: 2025/11/07 12:25:13 by brunogue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-
 Bureaucrat::Bureaucrat()
 {
 	std::cout << "Default constructor called" << std::endl;
 }
-
-
 
 Bureaucrat::~Bureaucrat()
 {
