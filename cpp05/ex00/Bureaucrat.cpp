f95050801e1738db03eb4b4a55b1d8af2179e143
Bruno Nogueira De Queiroz <bqueiroz.nog@gmail.com>
2025-11-22 19:23:37 -0300

    ex00 done

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -6,11 +6,31 @@
 /*   By: brunogue <brunogue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 19:02:08 by brunogue          #+#    #+#             */
-/*   Updated: 2025/11/22 18:42:38 by brunogue         ###   ########.fr       */
+/*   Updated: 2025/11/22 19:11:57 by brunogue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : _name("Default"), _grade(150)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other) : _name(other._name), _grade(other._grade)
+{
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other)
+{
+	if (this != &other)
+	{
+	this->_grade = other._grade;
+	}
+	std::cout << "Copy assignment operator called" << std::endl;
+	return *this;
+}
 
 Bureaucrat::Bureaucrat(const std::string &name, unsigned int grade) : _name(name), _grade(grade)
 {
@@ -22,7 +42,6 @@ Bureaucrat::Bureaucrat(const std::string &name, unsigned int grade) : _name(name
 	{
 		throw (Bureaucrat::GradeTooLowException());
 	}
-	std::cout << "Default constructor called" << std::endl;
 }
 
 Bureaucrat::~Bureaucrat()
