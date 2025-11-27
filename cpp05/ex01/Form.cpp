9aac2a644e3e7f968f03db9333ec95fb8dbaad4c
root <bqueiroz.nog@gmail.com>
2025-11-26 22:14:48 -0300

    added cpp05 ex02

--- cpp05/ex01/Form.cpp
+++ cpp05/ex01/Form.cpp
@@ -22,13 +22,7 @@ Form::Form(const std::string& name, int gradeToSign, int gradeToExecute)
         throw Form::GradeTooLowException();
 }
 
-Form::Form(const Form& other)
-    : _name(other._name),
-      _isSigned(other._isSigned),
-      _gradeToSign(other._gradeToSign),
-      _gradeToExecute(other._gradeToExecute)
-{
-}
+Form::Form(const Form& other) : _name(other._name), _isSigned(other._isSigned), _gradeToSign(other._gradeToSign), _gradeToExecute(other._gradeToExecute) {}
 
 Form& Form::operator=(const Form& other)
 {
